ff4e875c5434610fecccc32fa02b4ca82a605804
Gary Li <garyli@email.arizona.edu>
2021-04-16 12:39:22 -0700

    Created java classes for project

---
+++ src/Minesweeper.java
@@ -0,0 +1,11 @@
+import javafx.application.Application;
+import view.MinesweeperView;
+
+public class Minesweeper {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Application.launch(MinesweeperView.class);
+	}
+}
---
+++ src/view/MinesweeperView.java
@@ -0,0 +1,21 @@
+package view;
+
+import java.util.Observable;
+import java.util.Observer;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MinesweeperView extends Application implements Observer {
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void update(Observable o, Object arg) {
+		// TODO Auto-generated method stub
+	}
+	
+}
