d789f073fe3ac132186d8fd6c33626a24e170c00
Gary Li <garyli0806@gmail.com>
2021-05-05 15:35:00 -0700

    Finished comments for the minesweeper class and the controller class

--- src/Minesweeper.java
+++ src/Minesweeper.java
@@ -1,9 +1,17 @@
 import javafx.application.Application;
 import view.MinesweeperView;
 
+/**
+ * Creates a GUI based minesweeper game.
+ * 
+ * @author Gary Li, Daniel S. Lee, Robert Schnell, Merle Crutchfield
+ */
 public class Minesweeper {
+	
 	/**
-	 * @param args
+	 * Launches the Minesweeper game
+	 * 
+	 * @param args None required.
 	 */
 	public static void main(String[] args) {
 		Application.launch(MinesweeperView.class);
--- src/controller/MinesweeperController.java
+++ src/controller/MinesweeperController.java
@@ -3,13 +3,23 @@ package controller;
 import model.MinesweeperBoard;
 import model.MinesweeperModel;
 
+/**
+ * A controller class for the minesweeeper game. Holds a model and board instance 
+ * and offers methods to interact with the game model. Supports methods to reveal 
+ * a tile, flag a tile, reveal all mines on the board, determine if a save exists,
+ * determine if it's the first move, and determine if the game has been won or lost.
+ * 
+ * @author Gary Li, Daniel S. Lee, Robert Schnell, Merle Crutchfield
+ */
 public class MinesweeperController {
+	
 	private static MinesweeperBoard board;
 	private MinesweeperModel model;
 
 	/**
+	 * Constructs the controller with a model instance.
 	 * 
-	 * @param model
+	 * @param model The minesweeper model
 	 */
 	public MinesweeperController(MinesweeperModel model) {
 		this.model = model;
@@ -17,33 +27,9 @@ public class MinesweeperController {
 	}
 
 	/**
-	 * 
-	 */
-	public void printBoard() {
-		for (int i = 1; i <= 13; i++) {
-			System.out.println("  -----------------------------------------------------");
-			System.out.print(Integer.toString(i) + " | ");
-			for (int j = 1; j <= 13; j++) {
-				if (!board.getTile(i - 1, j - 1).inBounds) {
-					System.out.print("-" + " | ");
-				} else if (board.getTile(i - 1, j - 1).hasMine) {
-					System.out.print("x" + " | ");
-				} else if (board.getTile(i - 1, j - 1).displayNum != 0) {
-					System.out.print(board.getTile(i - 1, j - 1).displayNum + " | ");
-				} else {
-					System.out.print("o" + " | ");
-				}
-			}
-			System.out.println();
-		}
-		System.out.println("  -----------------------------------------------------");
-		System.out.println("    a   b   c   d   e   f   g   h   i   j   k   l   m");
-	}
-
-	/**
 	 * Returns whether the game has been won.
 	 * 
-	 * @return
+	 * @return a boolean that represents whether the game has been won or not
 	 */
 	public boolean hasWon() {
 		int tiles = 0;
@@ -58,19 +44,20 @@ public class MinesweeperController {
 	}
 
 	/**
+	 * Reveals a tile at the given coordinates
 	 * 
-	 * @param row
-	 * @param col
-	 * @throws GameLostException
+	 * @param row A row coordinate
+	 * @param col A column coordinate
 	 */
 	public void revealSpace(int row, int col) {
 		model.revealSpace(row, col);
 	}
 
 	/**
+	 * Flag the tile at the given coordinates
 	 * 
-	 * @param row
-	 * @param col
+	 * @param row A row coordinate
+	 * @param col A column coordinate
 	 */
 	public void flagSpace(int row, int col) {
 		model.flagSpace(row, col);
@@ -84,14 +71,29 @@ public class MinesweeperController {
 		model.revealMines();
 	}
 
+	/**
+	 * Return whether or not it is currently the first move
+	 * 
+	 * @return a boolean representing whether or not it is the first move
+	 */
 	public boolean isFirstMove() {
 		return model.getFirstMove();
 	}
 
+	/**
+	 * Returns whether or not a previous save exists
+	 * 
+	 * @return a boolean representing whether or not a previous save exists
+	 */
 	public boolean hasSave() {
 		return model.getSave();
 	}
 
+	/**
+	 * Returns whether or not the game has been lost
+	 * 
+	 * @return a boolean representing whether the game has been lost or not
+	 */
 	public boolean hasLost() {
 		return model.getLost();
 	}
